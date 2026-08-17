d36b4c1d3b95d8aa73fdd95660fef7db9ed63024
Guy Clement Kamdem <gycks.wolf555@slmail.m>
2026-08-17 21:11:33 +0200

    fix: lint issues

--- kordevance/application/dependencies/secret_store_adapter.py
+++ kordevance/application/dependencies/secret_store_adapter.py
@@ -2,9 +2,9 @@ from typing import Annotated
 
 from fastapi import Depends
 
+from kordevance.adapters.secret_store.credential_manager import CredentialManager
 from kordevance.application import _APPLICATION_NAME
 from kordevance.domain.ports.credential_manager import CredManager
-from kordevance.adapters.secret_store.credential_manager import CredentialManager
 
 
 def get_credential_manager() -> CredManager:
--- kordevance/domain/use_cases/device_management/__init__.py
+++ kordevance/domain/use_cases/device_management/__init__.py
@@ -1,3 +1,3 @@
 _SERVICE: str = "device"
 _ID_KEY: str = "device_id"
-_SECRET_KEY: str = "device_secret"
+_SECRET_KEY: str = "device_secret"  # noqa: S105
